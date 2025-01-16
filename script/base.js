dd9c038faa4dac129843408dc1d1bc4822b62061
enoch-bolarinwa <enoch-bolarinwa@>
2025-01-16 13:08:03 +0100

    index

--- script/base.js
+++ script/base.js
@@ -1,10 +1,4 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
 
 const courses = [
     {
